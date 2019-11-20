27d7f1d82d4a890567818ec86cc34f0930cbdc30
nathanveach <nathanbveach@gmail.com>
2019-11-20 14:48:15 -0800

    Styled checkout modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,38 @@
 //= require_tree .
 
 
+$(document).on('click', '#checkoutModal', function() {
+    $('#checkoutForm').validate({
+      // Specify validation rules
+      rules: {
+        // The key name on the left side is the name attribute
+        // of an input field. Validation rules are defined
+        // on the right side
+        firstname: "required",
+        lastname: "required",
+        country: "required",
+        address: "required",
+        city: "required",
+        state: "required",
+        zip: "required",
 
-  
-
+        email: {
+          required: true,
+          // Specify that email should be validated
+          // by the built-in "email" rule
+          email: true
+        },
+      },
+      // Specify validation error messages
+      messages: {
+        firstname: "Please enter your firstname",
+        lastname: "Please enter your lastname",
+        country: "Please select your country",
+        address: "Please enter a valid street address",
+        city: "Please enter your city",
+        state: "Please enter your state",
+        zip: "Please enter your postal code",
+        email: "Please enter a valid email address"
+      },
+    });
+  });
