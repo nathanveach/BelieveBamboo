74df540ecd259a5c15b1673e70fb80658a7b5941
nathanveach <nathanbveach@gmail.com>
2019-11-21 13:20:03 -0800

    Added products to seed file to push to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(title: 'Bamboo Drinking Straws [10 pk]', price: 10, description: 'These eco-friendly bamboo straws are not only sustainable but also completely bio-degradable. Perfect for events or parties these organic straws are sure to be a conversation starter!')
+Product.create(title: 'Bamboo Toothbrushes [5 pk]', price: 10, description: 'Say no more to plastic toothbrushes and prepare to take on the sustainable initiative with this 100% organic bamboo toothbrush. With 5 pieces included all individually wrapped these make perfect gifts for friends & family.')
+Product.create(title: 'Bamboo Cutlery Set [Full]', price: 15, description: 'This all inclusive all bamboo cutlery set is perfect for all your travel needs. Wrapped up in a floral patterned fabric these utensils are sure to bring the life to any picnic!')
