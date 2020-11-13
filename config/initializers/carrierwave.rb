a39f5a0a42762d85aadfdc316cb88bbdc5288b05
nathanveach <nathanbveach@gmail.com>
2020-11-13 12:41:16 -0800

    Updated credentials

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',                        # required
-    aws_access_key_id: ENV["aws_access_key_id"],                        # required unless using use_iam_profile
-    aws_secret_access_key: ENV["aws_secret_access_key"],   # required unless using use_iam_profile
+    aws_access_key_id: Rails.application.credentials.aws[:access_key_id],                        # required unless using use_iam_profile
+    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],   # required unless using use_iam_profile
     region: 'us-west-1'
   }
-  config.fog_directory  = ENV["fog_directory"]                                      # required
-end
+  config.fog_directory  = 'believebamboo'
